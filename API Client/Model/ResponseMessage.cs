f6e943b88b47637e0504bd4f8cb4d6b5d86151d0
Lv100Luca <luca@trenz.de>
2023-07-07 23:50:48 +0200

    created ResponseMessage.cs class

---
+++ API Client/Model/ResponseMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace API_Client.Model;
+
+public class ResponseMessage
+{
+    public ResponseMessage(string action, int status, string message)
+    {
+        Action = action;
+        Status = status;
+        this.Message = message;
+        
+    }
+    private string Action { get; set; }
+    private int Status { get; set; }
+    private string Message { get; set; }
+}
