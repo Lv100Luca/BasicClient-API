7c2e7d99e8ebcc60cf8d88790526e91adf64205a
Lv100Luca <luca@trenz.de>
2023-07-07 23:51:50 +0200

    added BasicEndpointsController.cs

---
+++ API Client/Controllers/BasicEndpointsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Client.Controllers;
+
+[ApiController]
+[Route("[controller]")] //todo controller/action
+public class BasicEndpointsController : ControllerBase
+{
+    private readonly ILogger<BasicEndpointsController> _logger;
+
+    public BasicEndpointsController(ILogger<BasicEndpointsController> logger)
+    {
+        _logger = logger;
+    }
+    #region Get-Requsts
+
+    [HttpGet("get/success")]
+    public IActionResult GetDataSuccess()
+    {
+        return Ok(new
+        {
+            name = "luca",
+            age = 20,
+            password = "123",
+        });
+    }
+
+    [HttpGet("get/unauthorized")]
+    public IActionResult GetDataUnauthorized()
+    {
+        return Ok(new
+        {
+            name = "luca",
+            age = 20,
+            password = "123",
+        });
+    }
+
+    #endregion
+}
