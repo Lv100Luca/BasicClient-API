3ab462a6d834c60d32b7cf011ee4fac47dfdd92f
Lv100Luca <luca@trenz.de>
2023-07-08 00:24:24 +0200

    added Post endpoints

--- API Client/Controllers/BasicEndpointsController.cs
+++ API Client/Controllers/BasicEndpointsController.cs
@@ -16,22 +16,73 @@ public class BasicEndpointsController : ControllerBase
     #region Get-Requests
 
     [HttpGet("get/success")]
-    public IActionResult GetDataSuccess()
+    public IActionResult GetSuccess()
     {
         return Ok(new ResponseMessage("GET", 200, "Success"));
 
     }
 
     [HttpGet("get/unauthorized")]
-    public IActionResult GetDataUnauthorized()
+    public IActionResult GetUnauthorized()
     {
         return Unauthorized(new ResponseMessage("GET", 401, "Unauthorized"));
 
-    }    [HttpGet("get/nocontent")]
-    public IActionResult GetDataNoContent()
+    }
+
+    [HttpGet("get/nocontent")]
+    public IActionResult GetNoContent()
     {
         return NoContent(); //no content requires empty response
 
+    }
+
+    #endregion
+
+    #region Post-Requests
+
+    [HttpPost("post/success")]
+    public IActionResult PostSuccess(IncomingDataDto incomingData)
+    {
+        return Ok(new ResponseMessage("POST", 200, $"Success: {incomingData.secret}"));
+    }
+
+    [HttpPost("post/unauthorized")]
+    public IActionResult PostUnauthorized(IncomingDataDto incomingData)
+    {
+        return Unauthorized(new ResponseMessage("POST", 401, $"Unauthorized: {incomingData.secret}"));
+    }
+
+    [HttpPost("post/nocontent")]
+    public IActionResult PostNoContent(IncomingDataDto incomingData)
+    {
+        _logger.LogTrace(incomingData.secret.ToString());
+        return NoContent(); //no content requires empty response
+    }
+
+    [HttpPost("post/nobody")]
+    public IActionResult PostNoBody()
+    {
+        return Ok(new ResponseMessage("POST", 200, "Success"));
+    }
+
+    [HttpPost("post/empty")]
+    public IActionResult PostEmpty()
+    {
+        return Ok();
+    }
+
+    [HttpPost("post/badrequest")]
+    public IActionResult PostBadRequest()
+    {
+        return BadRequest(new ResponseMessage("POST", 400, "Error"));
+    }
+
+    [HttpPost("post/error")]
+    public IActionResult PostInternalSeverError()
+    {
+        throw new Exception("Internal Server Error");
+        // ReSharper disable once HeuristicUnreachableCode
+        // return BadRequest(new ResponseMessage("POST", 400, "Error")); unreachable (on purpose)
     }
 
     #endregion
