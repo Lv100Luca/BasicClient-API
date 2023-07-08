c7cc185727580d894edfafe7af336b926c739a41
Lv100Luca <luca@trenz.de>
2023-07-08 21:12:16 +0200

    Added JWT setup and dependencies

--- API Client/Program.cs
+++ API Client/Program.cs
@@ -1,4 +1,7 @@
 using API_Client.Controllers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,8 +24,31 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // "jwtbearer"
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // "jwtbearer"
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; // for development
+    options.SaveToken = true; //saves the token in HttpContext, accessible with 'await HttpContext.GetTokenAsync("access_token");'
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, // check if the tokens Issuer matches
+        ValidateAudience = true, // check if the tokens Audience matches
+        ValidateLifetime = true, // check if the token is still valid and not expired
+        ValidateIssuerSigningKey = true, // weather to validate the signature of the token
+        ValidIssuer = "my_issuer", // Replace with your issuer
+        ValidAudience = "my_audience", // Replace with your audience
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Keqing")) // set an expected signature for the token
+    };
+});
+
 var app = builder.Build();
 
+// use JWT-Token for authentication
+app.UseAuthentication();
+app.UseAuthorization();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
