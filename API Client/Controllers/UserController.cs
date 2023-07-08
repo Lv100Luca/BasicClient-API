8d512e14e39e8d550f2eab9f99b1eae3002ff70a
Lv100Luca <luca@trenz.de>
2023-07-08 23:50:31 +0200

    renamed

---
+++ API Client/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using API_Client.Model;
+using API_Client.Model.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Client.Controllers;
+
+[ApiController]
+[Route("jwt")]
+public class UserController : ControllerBase
+{
+    private readonly JwtTokenService _jwtTokenService;
+    public UserController(JwtTokenService jwtTokenService)
+    {
+        _jwtTokenService = jwtTokenService;
+    }
+
+
+    [HttpPost("Login")]
+    [AllowAnonymous]
+    public IActionResult Login(UserLoginDto userLogin)
+    {
+        var user = UserDb.Authenticate(userLogin);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var token = _jwtTokenService.GenerateToken(user);
+        return Ok(token);
+    }
+
+    
+    [HttpGet("/admin")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetAllUsers()
+    {
+        return Ok(UserDb.GetAllUsers());
+    }
+
+
+    [HttpGet("/test")]
+    [Authorize(Roles = "User")]
+    public IActionResult GetMessage()
+    {
+        return Ok("Hello World!");
+    }
+}
