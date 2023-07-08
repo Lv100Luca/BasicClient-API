f5698970b269eb72efc2895ec9fc3a2e03291b67
Lv100Luca <luca@trenz.de>
2023-07-08 21:50:41 +0200

    added MyDbServiceImplementation.cs as a temporary replacement for a proper DataBase, that will act as one

---
+++ API Client/Model/MyDbServiceImplementation.cs
@@ -0,0 +1,47 @@
+﻿namespace API_Client.Model;
+
+public class MyDbServiceImplementation // todo implement proper DB 
+{
+    private static List<User> users = new List<User>
+    {
+        new User("Loeka", "Keqing", "Admin"),
+        new User("Cinnamonroll", "Sakana", "User"),
+        new User("Pikachu", "Pikachu", "User"),
+        new User("Eevee", "Eevee", "User"),
+        new User("Bulbasaur", "Bulbasaur", "User"),
+    };
+
+    //crazy comment
+    /// <summary>
+    /// Retrieves a user from the list of users based on the provided username.
+    /// </summary>
+    /// <param name="username">The username to search for.</param>
+    /// <returns>The user with the specified username, or null if no user is found.</returns>
+    public static User? GetUserByUsername(string username)
+    {
+        var user = users.Find(user => user.Username == username);
+        if (user == null)
+        {
+            return null;
+        }
+        return user;
+    }
+
+    public static bool DeleteUser(string username)
+    {
+        var user = users.Find(user => user.Username == username);
+        if (user == null)
+        {
+            return false;
+        }
+        users.Remove(user);
+        return true;
+    }
+
+    public static bool AddUser(User user)
+    {
+        users.Add(user);
+        return true;
+    }
+
+}
