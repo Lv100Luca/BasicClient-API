2a4c9a8083c9d35aba03ffaefe178a913e4da774
Lv100Luca <luca@trenz.de>
2023-07-08 23:10:12 +0200

    Added Swagger JWT Configuration

--- API Client/Program.cs
+++ API Client/Program.cs
@@ -1,8 +1,10 @@
 using API_Client.Controllers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
+//ask - why is this format diffrent to the ones the KaRa API has (Classes) while this is more like script format
 var builder = WebApplication.CreateBuilder(args);
 
 // load appsettings.json (idk if this is explicitly needed)
@@ -47,7 +49,38 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? "FallbackString")) // set an expected signature for the token
     };
 });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Your API",
+            Version = "v1"
+        });
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme.",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 
+});
 var app = builder.Build();
 
 // use JWT-Token for authentication
@@ -59,7 +92,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.UseCors();
 app.UseAuthorization();
